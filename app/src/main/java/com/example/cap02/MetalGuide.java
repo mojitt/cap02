bdd11d50ab9487bddbba59538336a4e2ec4548f9
mojitt <https://github.com/mojitt/mojitt.git>
2022-06-23 16:06:53 +0900

    Initial commit

---
+++ app/src/main/java/com/example/cap02/MetalGuide.java
@@ -0,0 +1,15 @@
+package com.example.cap02;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MetalGuide extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.metal_guide);
+
+    }
+}
