3871935f0af3e809418c881dbc53a659a0576248
mojitt <https://github.com/mojitt/mojitt.git>
2022-06-25 15:58:49 +0900

    asdasd

--- app/src/main/java/com/example/cap02/GuideHome.java
+++ app/src/main/java/com/example/cap02/GuideHome.java
@@ -3,20 +3,47 @@ package com.example.cap02;
 import android.app.TabActivity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TabHost;
+import android.widget.TextView;
 
-public class GuideHome extends TabActivity {
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.firebase.firestore.DocumentChange;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GuideHome extends AppCompatActivity implements View.OnClickListener {
 
     Button Btnplastic, Btncan, Btnpaper,Btnmirror, Btnmetal, Btnstyro, Btnvinyl;
+
+    TabHost tabHost;
+
+    private RecyclerView mMainRecyclerView;
+
+    private MainAdapter mAdapter;
+    private List<Board> mBoardList;
+
+    private FirebaseFirestore mStore = FirebaseFirestore.getInstance();
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.guide_home);
 
-        TabHost tabHost = getTabHost();
+        tabHost = (TabHost) findViewById(R.id.tabhost);
+        tabHost.setup();
 
         TabHost.TabSpec tabSpecCalendar = tabHost.newTabSpec("product").setIndicator("분리수거 하기");
         tabSpecCalendar.setContent(R.id.content1);
@@ -35,6 +62,30 @@ public class GuideHome extends TabActivity {
         Btnmetal = (Button) findViewById(R.id.metalbtn);
         Btnstyro = (Button) findViewById(R.id.styrobtn);
         Btnvinyl = (Button) findViewById(R.id.vinylbtn);
+
+        mMainRecyclerView = findViewById(R.id.main_recycler_view);
+
+        findViewById(R.id.main_write_button).setOnClickListener(this);
+
+        mBoardList = new ArrayList<>();
+
+        mStore.collection("board").addSnapshotListener(new EventListener<QuerySnapshot>() {
+            @Override
+            public void onEvent(@Nullable QuerySnapshot queryDocumentSnapshots, @Nullable FirebaseFirestoreException error) {
+                for (DocumentChange dc : queryDocumentSnapshots.getDocumentChanges()) {
+                    String id = (String) dc.getDocument().getData().get("id");
+                    String title = (String) dc.getDocument().getData().get("title");
+                    String contents = (String) dc.getDocument().getData().get("contents");
+                    String name = (String) dc.getDocument().getData().get("name");
+                    Board data = new Board(id, title, contents, name);
+
+                    mBoardList.add(data);
+                }
+
+                mAdapter = new MainAdapter(mBoardList);
+                mMainRecyclerView.setAdapter(mAdapter);
+            }
+        });
 
         Btnplastic.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
@@ -84,6 +135,53 @@ public class GuideHome extends TabActivity {
                 startActivity(intent);
             }
         });
+    }
+
+    @Override
+    public void onClick(View v) {
+        startActivity(new Intent(this, WriteActivity.class));
+
+    }
+
+
+    private class MainAdapter extends RecyclerView.Adapter<MainAdapter.MainViewHolder> {
+
+        private List<Board> mBoardList;
+
+        public MainAdapter(List<Board> mBoardList) {
+            this.mBoardList = mBoardList;
+        }
+
+        @NonNull
+        @Override
+        public MainViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            return new MainViewHolder(LayoutInflater.from(parent.getContext()).inflate(R.layout.item_main,parent, false));
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull MainViewHolder holder, int position) {
+            Board data = mBoardList.get(position);
+            holder.mTitleTextVew.setText(data.getTitle());
+            holder.mNameTextView.setText(data.getName());
+        }
+
+        @Override
+        public int getItemCount() {
+            return mBoardList.size();
+        }
+
+        class MainViewHolder extends RecyclerView.ViewHolder{
+
+            private TextView mTitleTextVew;
+            private TextView mNameTextView;
+
+            public MainViewHolder(View itemView) {
+                super(itemView);
+
+                mTitleTextVew = itemView.findViewById(R.id.item_title_text);
+                mNameTextView = itemView.findViewById(R.id.item_name_text);
+            }
+        }
     }
 
 }
---
+++ app/src/main/java/com/example/cap02/WriteActivity.java
@@ -0,0 +1,67 @@
+package com.example.cap02;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class WriteActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private FirebaseFirestore mStore = FirebaseFirestore.getInstance();
+
+    private EditText mWriteTitleText, mWriteContentsText, mWriteNameText;
+
+    private String id;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_write);
+
+        mWriteTitleText = findViewById(R.id.write_title_text);
+        mWriteContentsText = findViewById(R.id.write_contents_text);
+        mWriteNameText = findViewById(R.id.write_name_text);
+
+        findViewById(R.id.write_upload_button).setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        id = mStore.collection("board").document().getId();
+
+        Map<String, Object> post = new HashMap<>();
+        post.put("id", id);
+        post.put("title", mWriteTitleText.getText().toString());
+        post.put("contents", mWriteContentsText.getText().toString());
+        post.put("name", mWriteNameText.getText().toString());
+
+        mStore.collection("board").document(id).set(post)
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void unused) {
+                        Toast.makeText(WriteActivity.this, "업로드 성공!", Toast.LENGTH_SHORT).show();
+                        finish();
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(WriteActivity.this, "업로드 실패!", Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+    }
+
+}
